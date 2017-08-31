2dc5af95f7764706ee91cb8bec429cf4c8ffa26a
@zouyajun <@zouyajunzyj@sina.com>
2017-08-31 14:30:33 +0800

    - 创建工程

---
+++ LoveTooth/LoveTooth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoveTooth
+//
+//  Created by Ryan on 2017/8/31.
+//  Copyright © 2017年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
